8546e3592b61832b2bb810161e644dce2125b2c6
T-Tommasi <ttommasi1945@gmail.com>
2024-10-29 18:16:00 +0100

    (FEAT) Added game logic
    - Added basics functions for game logic
    - Added calls to the DOM
    - Finished styling the .screen and relative img's
    - Added basic logic for starting a new round
    
    (TODO)
    
    - Add round counter
    - Add W/L counter
    - Add graphics for the game

--- script.js
+++ script.js
@@ -1,2 +1,65 @@
-const startGame = document.querySelector('#newGame')
-const reset = document.querySelector('#reset')
+//Game controls
+const startGame = document.querySelector('#newGame');
+const reset = document.querySelector('#reset');
+const selections = ['rock', 'paper', 'scissors']
+
+//Controls for the game
+const selectionButtons = document.querySelectorAll('.gameBoard>button')
+
+//The actual game board to draw the player and computer choices
+const display = document.querySelector('.screen');
+let round = document.querySelector('#roundNumber');
+let playerScore = document.querySelector('#pcWon');
+let playerLoss = document.querySelector('#pcLoss');
+
+
+//Various variables usefull for production, to be categorized later on
+let playerChoice = null;
+let computerChoice = '';
+let status = '';
+
+//Function for randomly selecting the AI play
+function pcChoice() {
+    let choice = Math.floor(Math.random()*selections.length);
+    let computerSelection = null
+    if (choice === 0) {
+        computerSelection = scissors
+    } else if (choice === 1) {
+        computerSelection = paper
+    } else if (choice === 2) {
+        computerSelection = rock
+    }
+    return computerSelection
+}
+
+function drawSelection(sign) {
+    
+}
+
+function playRound (playerChoice) {
+    computerSelection = pcChoice();
+    if (computerSelection === scissors && playerChoice == rock || computerSelection == rock && playerChoice == paper || computerSelection == paper && playerChoice == scissors ) {
+        win();
+        return;
+    } else if (computerSelection === playerChoice) {
+        draw();
+        return;
+    } else if (computerSelection === rock && playerChoice === scissors || computerSelection === paper && playerChoice === rock || computerSelection === scissors && playerChoice == paper) {
+        lose();
+        return;
+    }
+}
+
+//Function for player choice
+
+function registerChoice () {
+    if(this == paper) {
+        playerChoice = 'Paper!'
+    } else if (this == rock) {
+        playerChoice = 'Rock!'
+    } else if (this == scissors) {
+        playerChoice = 'Scissors!'
+    }
+}
+
+selectionButtons.forEach(selectionButtons => {selectionButtons.addEventListener('click', registerChoice)});
